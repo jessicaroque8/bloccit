dd4d27a801b395f0041cc89a44e0a210db34b651
Jessica Roque <jroque.dev@gmail.com>
2017-12-05 16:32:23 -0800

    Assignment17: Added censor feature by adding functionality to the index method in PostsController.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,14 +1,3 @@
 class Post < ApplicationRecord
    has_many :comments
-
-   def censor
-     if self.id == 1
-        self.title == 'SPAM'
-     elsif (self.id - 1) % 5 == 0
-         self.title == 'SPAM'
-      end
-   end
-
-   self.censor
-
 end
