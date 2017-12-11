718f162bafccfbd9f809b8ed1fd747d41f32e9e8
Jessica Roque <jroque.dev@gmail.com>
2017-12-11 15:04:42 -0800

    Checkpoint 27: Added authorization with roles for members and admin. Updated UI.

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,11 +34,19 @@ users = User.all
    )
  end
 
- user = User.first
- user.update_attributes!(
-    email: 'jroque.dev@gmail.com',
-    password: 'helloworld'
-)
+admin = User.create!(
+   name:     'Admin User',
+   email:    'admin@example.com',
+   password: 'helloworld',
+   role:     'admin'
+ )
+
+ # Create a member
+ member = User.create!(
+   name:     'Member User',
+   email:    'member@example.com',
+   password: 'helloworld'
+ )
 
  puts "Seed finished"
  puts "#{User.count} users created"
