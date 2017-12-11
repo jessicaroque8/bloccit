0ba077920843fabcd35ae1ff41bffcef5da14af8
Jessica Roque <jroque.dev@gmail.com>
2017-12-11 13:21:47 -0800

    Checkpoint 26: Associated users with posts. Updated views and ordered posts by creation date.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,14 @@
 require 'random_data'
+
+5.times do
+  User.create!(
+  name:     RandomData.random_name,
+  email:    RandomData.random_email,
+  password: RandomData.random_sentence
+  )
+end
+users = User.all
+
 15.times do
    Topic.create!(
      name:         RandomData.random_sentence,
@@ -9,6 +19,7 @@ require 'random_data'
 
  50.times do
    Post.create!(
+     user: users.sample,
      topic: topics.sample,
      title:  RandomData.random_sentence,
      body:   RandomData.random_paragraph
@@ -23,7 +34,14 @@ require 'random_data'
    )
  end
 
+ user = User.first
+ user.update_attributes!(
+    email: 'jroque.dev@gmail.com',
+    password: 'helloworld'
+)
+
  puts "Seed finished"
+ puts "#{User.count} users created"
  puts "#{Topic.count} topics created"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
