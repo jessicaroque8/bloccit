3426c16c0fed53753d88bc3afbee31c33d4ec54f
Jessica Roque <jroque.dev@gmail.com>
2017-12-11 19:21:45 -0800

    Removed merge conflict markers.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
      @user.email = params[:email]
      @user.password = params[:password]
      @user.password_confirmation = params[:password_confirmation]
+   end
 
    def create
      @user = User.new
