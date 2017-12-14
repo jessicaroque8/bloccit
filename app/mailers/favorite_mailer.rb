8c97ab8b2fcec55028452334825e8a3501d0288c
Jessica Roque <jroque.dev@gmail.com>
2017-12-13 16:13:59 -0800

    Assignment 30: Automated a post creator's favorite on their post after creation and a confirmation email.

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -3,14 +3,24 @@ class FavoriteMailer < ApplicationMailer
 
    def new_comment(user, post, comment)
 
-     headers["Message-ID"] = "<comments/#{comment.id}@your-app-name.example>"
-     headers["In-Reply-To"] = "<post/#{post.id}@your-app-name.example>"
-     headers["References"] = "<post/#{post.id}@your-app-name.example>"
+     headers["Message-ID"] = "<comments/#{comment.id}@lit-escarpment-47834.herokuapp.com>"
+     headers["In-Reply-To"] = "<post/#{post.id}@lit-escarpment-47834.herokuapp.com>"
+     headers["References"] = "<post/#{post.id}@lit-escarpment-47834.herokuapp.com>"
 
      @user = user
      @post = post
      @comment = comment
 
      mail(to: user.email, cc: "jessicaroque4894@gmail.com", subject: "New comment on #{post.title}")
+   end
+
+   def new_post(post)
+     headers["Message-ID"] = "<post/#{post.id}@lit-escarpment-47834.herokuapp.com>"
+     headers["In-Reply-To"] = "<post/#{post.id}@lit-escarpment-47834.herokuapp.com>"
+     headers["References"] = "<post/#{post.id}@lit-escarpment-47834.herokuapp.com>"
+
+     @post = post
+
+     mail(to: post.user.email, cc: "jessicaroque4894@gmail.com", subject: "You'll receive updates on your new post: #{post.title}")
    end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -12,6 +12,8 @@ class Post < ApplicationRecord
    validates :topic, presence: true
    validates :user, presence: true
 
+   after_create :favorite_post, :email_user
+
    def up_votes
      votes.where(value: 1).count
    end
@@ -28,6 +30,16 @@ class Post < ApplicationRecord
      age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
      new_rank = points + age_in_days
      update_attribute(:rank, new_rank)
+   end
+
+   private
+
+   def favorite_post
+      user.favorites.create!(post: self)
+   end
+
+   def email_user
+      FavoriteMailer.new_post(self).deliver_now
    end
 
 end
